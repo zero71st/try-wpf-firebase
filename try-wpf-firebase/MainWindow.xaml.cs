ffad601ac68862daa5b1848e121ddbe26bc001eb
Kasem Wongsa <zero71st@gmail.com>
2018-09-29 21:12:12 +0700

    Added function set,push,get,update,delete to firebase

--- try-wpf-firebase/MainWindow.xaml.cs
+++ try-wpf-firebase/MainWindow.xaml.cs
@@ -41,40 +41,84 @@ namespace MySchool_Firebase
 
             this.Loaded += MainWindow_Loaded;
             btSet.Click += btSet_Click;
+            btPush.Click += btPush_Click;
+            btGet.Click +=btGet_Click;
+            btUpdate.Click += btUpdate_Click;
             btDelete.Click += btDelete_Click;
+        }
+        async void btGet_Click(object sender, RoutedEventArgs e)
+        {
+            string studentID = tbID.Text;
+
+            FirebaseResponse response = await _client.GetAsync("students/" + tbID.Text);
+
+            Student student = response.ResultAs<Student>();
+
+            lbResponse.Text = string.Format("Found {0} {1} {2} in realtime database",student.ID,student.Name,student.SureName);
+        }
+
+        async void btUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var student = GetStudent();
+
+            FirebaseResponse response = await _client.UpdateAsync("students/" + student.ID, student);
+
+            lbResponse.Text = String.Format("Updated {0}",response.ResultAs<Student>().Name);
+        }
+
+        async void btPush_Click(object sender, RoutedEventArgs e)
+        {
+            var student = GetStudent();
+
+            PushResponse response = await _client.PushAsync("students/", student);
+
+            lbResponse.Text = response.Result.name;
         }
 
         async void btDelete_Click(object sender, RoutedEventArgs e)
         {
             FirebaseResponse response = await _client.DeleteAsync("students/" + tbID.Text);
 
-            
+            lbResponse.Text = response.StatusCode.ToString();
         }
 
         async void btSet_Click(object sender, RoutedEventArgs e)
         {
-            Student student = new Student
-            {
-                ID = tbID.Text,
-                Name = tbName.Text,
-                SureName = tbSername.Text,
-            };
+
+            var student = GetStudent();
 
             SetResponse response = await _client.SetAsync("students/"+student.ID,student);
 
+            lbResponse.Text = String.Format("Added {0}",response.ResultAs<Student>().Name);
         }
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             _client = new FirebaseClient(_config);
+        }
 
+        Student GetStudent()
+        {
+            return new Student(Convert.ToInt32(tbID.Text), tbName.Text, tbSername.Text);
         }
 
         public class Student
         {
-            public string ID { get; set; }
+            public int ID { get; set; }
             public string Name { get; set; }
             public string SureName { get; set; }
+
+            public Student()
+            {
+
+            }
+
+            public Student(int id,string name,string surename):this()
+            {
+                ID = id;
+                Name = name;
+                SureName = surename;
+            }
         }
     }
 }
